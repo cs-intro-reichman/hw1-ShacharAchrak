42a39a09af28493d5f03acc0f1d5477f8ece34c1
ShacharAchrak <shachar.achrak@post.runi.ac.il>
2024-11-08 13:01:43 +0200

    Bill3

--- Bill3.java
+++ Bill3.java
@@ -9,7 +9,7 @@ public class Bill3 {
 
 	    double billd = (double) bill;
 
-		System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + ": pay " + Math.ceil(billd / 3) + " Shekels each");   
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + Math.ceil(billd / 3) + " Shekels each");   
 	  }
    }
 
